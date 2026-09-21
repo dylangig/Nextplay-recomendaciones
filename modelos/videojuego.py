52a8606094d4273421fc55680f8ee1364020a685
nicolasmcostantini <costantininicolas2@gmail.com>
2026-09-21 14:37:06 -0300

    Corrige el acceso a atributos en la clase Videojuego para usar encapsulamiento

--- modelos/videojuego.py
+++ modelos/videojuego.py
@@ -1,18 +1,20 @@
 class Videojuego:
-    def __init__(self, titulo, genero, estudio, rating):
-        self._titulo = titulo
-        self._genero = genero
-        self._estudio = estudio
-        self._rating = rating
+    def __init__(self, titulo, genero, estudio, rating:float):
+        self.__titulo = titulo
+        self.__genero = genero
+        self.__estudio = estudio
+        self.__rating = rating
 
     def get_titulo(self):
-        return self._titulo
+        return self.__titulo
 
     def get_genero(self):
-        return self._genero
+        return self.__genero
 
     def get_rating(self):
-        return self._rating
+        return self.__rating
 
     def __repr__(self):
-        return f"{self._titulo} ({self._genero}) - Estudio: {self._estudio}  {self._rating}"
+        return f"{self.__titulo} ({self.__genero}) - Estudio: {self.__estudio}  {self.__rating}"
+
+
